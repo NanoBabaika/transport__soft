7eda11e6ffe952be8d95307b40a486d0748d1edc
Alexander <117478092+NanoBabaika@users.noreply.github.com>
2024-11-12 17:50:09 +0300

    Add files via upload

--- src/slider.js
+++ src/slider.js
@@ -1,6 +1,23 @@
-$('.slider').bxSlider({
-    minSlides: 3,
-    maxSlides: 3,
-    slideWidth: 431,
-    slideMargin:0
-  });
+
+const slider = $('.slider').bxSlider({
+      minSlides: 3,
+      maxSlides: 3,
+      slideWidth: 431,
+      slideMargin:0,
+      pager:false,
+      controls:false
+});
+
+
+
+$(".arrow-left").click(e => {
+  e.preventDefault();
+
+  slider.goToPrevSlide();
+})
+
+
+$(".arrow-right").click(e => {
+  e.preventDefault();
+  slider.goToNextSlide();
+})
